bf3410f2862864a74352565ca193a04ee422f8aa
uuboyscy <aegis12321@gmail.com>
2024-05-11 11:05:28 +0800

    partition table statement

--- gcp/sample_04_external_partition_table.sql
+++ gcp/sample_04_external_partition_table.sql
@@ -25,3 +25,21 @@ OPTIONS (
   skip_leading_rows = 1,
   max_bad_records = 1
 );
+
+---
+
+CREATE OR REPLACE EXTERNAL TABLE `demo.SalePartitionExternal`
+(
+  TransactionID STRING,
+  ProductID STRING,
+  Quantity INT64,
+  SaleDate DATE,
+)
+WITH PARTITION COLUMNS
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://tir101-demo/demo_partition/*'],
+  hive_partition_uri_prefix = 'gs://tir101-demo/demo_partition',
+  skip_leading_rows = 1,
+  max_bad_records = 1
+);
